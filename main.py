de32897d1625dbc2b7106e7222e6471b637020cf
Kasha <liadky@gmail.com>
2022-06-08 20:06:40 +0300

    Main Updates

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
 from Communication.Managers.ModuleUI import TModuleUIForm
 from Communication.Managers.ModuleManager import ModuleManager
 
-"""
-server_publisher.publish_msg(imu_report.SerializeToString(), topic=imu_report.DESCRIPTOR.name.encode())
-server_publisher.publish_msg(status_report.SerializeToString(), topic=status_report.DESCRIPTOR.name.encode())
-server_publisher.publish_msg(detection_report.SerializeToString(), topic=detection_report.DESCRIPTOR.name.encode())
-server_publisher.publish_msg(engine.preset_response_msg.SerializeToString(),
-                                         topic=engine.preset_response_msg.DESCRIPTOR.name.encode())
-"""
 if __name__ == '__main__':
  
     try:
         oModulesManager = ModuleManager()
         oModulesManager.start()
-        oTModuleUIForm = TModuleUIForm()
+        oTModuleUIForm = TModuleUIForm() # When user choose to exit App, oModulesManager.stop() will do clean exit (close threads)
         oModulesManager.stop()
     except KeyboardInterrupt:
-        oModulesManager.handle_cleanup(1)
+        oModulesManager.handle_cleanup(1) # Catch CTRL + C
